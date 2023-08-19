11ba21ed9bbdba39345eb22621cc436173760407
wilberquito <wbq.software@gmail.com>
2023-08-19 10:20:01 +0200

    Adds ohmyzsh install script

---
+++ install/ohmyzsh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ██████╗ ███████╗ █████╗ ██████╗ ███╗   ███╗███████╗
+# ██╔══██╗██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔════╝
+# ██████╔╝█████╗  ███████║██║  ██║██╔████╔██║█████╗
+# ██╔══██╗██╔══╝  ██╔══██║██║  ██║██║╚██╔╝██║██╔══╝
+# ██║  ██║███████╗██║  ██║██████╔╝██║ ╚═╝ ██║███████╗
+# ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚══════╝
+#
+# Author: wilberquito
+# License: WTFPL
+# Description:
+# 	- Framework for managing zsh configuration
+# Requirements:
+# 	- curl
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
