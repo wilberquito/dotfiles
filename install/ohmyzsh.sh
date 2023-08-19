8248751dedd6b87db65f7a02cad688a9b9830912
wilberquito <wbq.software@gmail.com>
2023-08-19 11:14:13 +0200

    Better way to install ohmyzsh and plugins

--- install/ohmyzsh.sh
+++ install/ohmyzsh.sh
@@ -10,8 +10,24 @@
 # Author: wilberquito
 # License: WTFPL
 # Description:
-# 	- Framework for managing zsh configuration
+# 	- Install a framework for managing zsh plugins.
+# 	- At the same time it install a bunch of plugins.
 # Requirements:
 # 	- curl
+# 	- git
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ZSH="$HOME/.oh-my-zsh"
+
+if [ ! -d "$ZSH" ]
+then
+  echo 'zsh'
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+ZSHAUTOSUGGESTIONS=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+if [ ! -d "$ZSHAUTOSUGGESTIONS" ]
+then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
